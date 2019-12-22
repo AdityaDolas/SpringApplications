deb37cab235d6acd59723f69814f96222acc485c
Aditya Dolas <dolasadi@gmail.com>
2019-12-22 18:29:35 +0530

    FC

---
+++ Spring-foundations/src/main/java/config/AppConfig.java
@@ -0,0 +1,19 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+	
+}
+
+/*
+ * public OrderService orderService(InventoryService inventoryService,
+ * CustomerRepository customerRepository, SalesOrderRepository
+ * salesOrderRepository) { return new OrderServiceImp(inventoryService,
+ * customerRepository, salesOrderRepository); }
+ * 
+ * @Bean public Inventory
+ */
